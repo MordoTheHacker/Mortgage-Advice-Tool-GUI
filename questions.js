125454de1c18080a221bae5033d8c41dcd6c829d
MordoTheHacker <91093022+MordoTheHacker@users.noreply.github.com>
2023-12-25 10:43:26 +0100

    Survey Page Dynamic Question display

--- questions.js
+++ questions.js
@@ -10,7 +10,15 @@ const questions = [
             'I want to evaluate my current situation', 
             'I want to buy a new house that needs renovating', 
             'My partner and I are seperating and I want to continue living in my current house'
-        ] 
+        ] ,
+        option_values: [
+            "existingHousePurchase",
+            "houseRenovation",
+            "mortgageRefinance",
+            "currentSituationEvaluation",
+            "newConstructionHousePurchase",
+            "postSeperationHouseContinuance"
+        ]
     },
     { 
         question_id: 2,
@@ -56,27 +64,139 @@ const questions = [
         ] 
     },
     { 
-        question_id: 1,
-        question: 'Which of the following situations apply to you?', 
+        question_id: 7,
+        question: 'Do you want to be able to cancel the mortgege quote free of charge even after signing it?', 
         options: [
-            'I want to buy a new house', 
-            'I want to renovate my house', 
-            'I want to refinance my mortgage', 
-            'I want to evaluate my current situation', 
-            'I want to buy a new house that needs renovating', 
-            'My partner and I are seperating and I want to continue living in my current house'
+            'Yes', 
+            'No', 
         ] 
     },
     { 
-        question_id: 1,
-        question: 'Which of the following situations apply to you?', 
+        question_id: 8,
+        question: 'Do you want to be able to make penalty-free repayments of more than 10% per year?', 
         options: [
-            'I want to buy a new house', 
-            'I want to renovate my house', 
-            'I want to refinance my mortgage', 
-            'I want to evaluate my current situation', 
-            'I want to buy a new house that needs renovating', 
-            'My partner and I are seperating and I want to continue living in my current house'
+            'Yes',
+            'No'
+        ] 
+    },
+    { 
+        question_id: 9,
+        question: 'Do you want to have the option to transfer the mortgage interest to a future property?', 
+        options: [
+            'Yes',
+            'No'
+        ] 
+    },
+    { 
+        question_id: 10,
+        question: 'Do you want a mortgage that automatically lowers interest rate when making extra repayments?', 
+        options: [
+            'Yes',
+            'No'
+        ] 
+    },
+    { 
+        question_id: 11,
+        question: 'What is your objective in case your partner passes away during the mortgage term?', 
+        options: [
+            'I want to continue living in the property without mortgage expenses', 
+            'I want to continue living in the property', 
+            'I will sell the property', 
+        ] 
+    },
+    { 
+        question_id: 12,
+        question: 'What is your objective in case you or your partner become disabled during the mortgage term?', 
+        options: [
+            'I want to continue living in the property', 
+            'I will sell the property', 
+            'Already have dissability'
+        ] 
+    },
+    { 
+        question_id: 13,
+        question: 'What is your objective in case you or your partner become unenemployed during the mortgage term?', 
+        options: [
+            'I want to continue living in the property', 
+            'I will sell the property', 
+            'Already unemployed'
+        ] 
+    },
+    { 
+        question_id: 14,
+        question: 'What is your objective when you or your partner retire?', 
+        options: [
+            'I want to continue living in the property', 
+            'I will sell the property', 
+            'Already retired',
+            'Alleen mijn inkomen',
+            'Alleen het inkomen van mijn partner',
+            'Zowel het inkomen van mijn partner als van mij'
+        ] 
+    },
+    { 
+        question_id: 15,
+        question: 'What is your experience with mortgages?', 
+        options: [
+            'I have a mortgage and know well what obligations I have entered into', 
+            'I have a mortgage but don\'t really know the obligations I have entered into', 
+            'I have had a mortgage in the past but not anymore', 
+            'I have no experience with taking out a mortgage', 
+            'I deal with mortgages in my work', 
+        ] 
+    },
+    { 
+        question_id: 16,
+        question: 'How familiar are you with an Interest-Only type of loan?', 
+        options: [
+            'Not familiar', 
+            'Limitedly familiar', 
+            'Fully familiar', 
+        ] 
+    },
+    { 
+        question_id: 17,
+        question: 'How familiar are you with an Annuity loan?', 
+        options: [
+            'Not familiar', 
+            'Limitedly familiar', 
+            'Fully familiar', 
+        ] 
+    },
+    { 
+        question_id: 18,
+        question: 'How familiar are you with a Bank svaings type of loan?', 
+        options: [
+            'Not familiar', 
+            'Limitedly familiar', 
+            'Fully familiar', 
+        ] 
+    },
+    { 
+        question_id: 19,
+        question: 'How familiar are you with an Invesments type of loan?', 
+        options: [
+            'Not familiar', 
+            'Limitedly familiar', 
+            'Fully familiar', 
+        ] 
+    },
+    { 
+        question_id: 20,
+        question: 'How familiar are you with an Life type of loan?', 
+        options: [
+            'Not familiar', 
+            'Limitedly familiar', 
+            'Fully familiar', 
+        ] 
+    },
+    { 
+        question_id: 21,
+        question: 'How familiar are you with a Linear loan?', 
+        options: [
+            'Not familiar', 
+            'Limitedly familiar', 
+            'Fully familiar', 
         ] 
     },
     { 
--- survey-script.js
+++ survey-script.js
@@ -1,15 +1,106 @@
-// Control progress bar
-const progressBar = document.getElementsByClassName('progressive_bar')[0];
-var compound = 1;
-const id = setInterval(() => {
-    const computedStyle = getComputedStyle(progressBar);
-    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
-    if(compound > 70){
-        clearInterval(id);
+import questions from './questions.js';
+
+var current_question_id = 0;
+const totalNrQuestions = 21;
+
+// Function to control progress bar
+function updateProgressBar(){
+    if(current_question_id <= totalNrQuestions - 1){
+        const progressBar = document.getElementsByClassName('progressive_bar')[0];
+        var updateInterval = ((current_question_id + 1) / totalNrQuestions) * 100; 
+        progressBar.style.setProperty('--width', updateInterval);
     }
-    progressBar.style.setProperty('--width', compound);
-    compound += width;
-}, 100);
+}
+
+//control question flow
+function updateUI(current_question_id_pass) {
+    current_question_id = current_question_id_pass;
+    const questionTitle = document.querySelector('#Question_Title span');
+    const optionContainer = document.getElementById('Option_Container');
+    //Update progress bar
+    updateProgressBar();
+    // Display Question Title
+    displayQuestionTitle(questions[current_question_id].question);
+
+    // Display Question options
+    displayQuestionOptions(questions[current_question_id].options, current_question_id);
+}
+
+// Function to display the question title and handle styling
+function displayQuestionTitle(title) {
+    const questionTitleElement = document.getElementById('Question_Title');
+    const questionTitle = questionTitleElement.querySelector('span');
+    const textLength = title.length;
+
+    updateTitleStyling(textLength);
+
+    questionTitle.textContent = title;
+}
+
+// Function to update title styling based on text length
+function updateTitleStyling(textLength) {
+    const questionTitleElement = document.getElementById('Question_Title');
+    const questionTitle = questionTitleElement.querySelector('span');
+    if (textLength > 50) {
+        if(textLength > 100) {
+            var newSize = textLength / 60;
+            questionTitleElement.style.lineHeight = "2.3vw";
+            questionTitle.style.fontSize = newSize + "vw";
+        } else {
+            var newSize = 140 / textLength;
+            questionTitleElement.style.lineHeight = "2.3vw";
+            questionTitle.style.fontSize = newSize + "vw";
+        }
+    } else {
+        questionTitleElement.style.lineHeight = "normal";
+        questionTitle.style.fontSize = "2.7vw";
+    }
+    
+}
+
+// Function to display question options
+function displayQuestionOptions(options, current_question_id) {
+    const optionContainer = document.getElementById('Option_Container');
+
+    // Clear existing options
+    optionContainer.innerHTML = '';
+
+    // Display new options
+    for (let i = 0; i < options.length; i++) {
+        const addOption = document.createElement('a');
+        addOption.href = '';
+        addOption.className = 'option';
+        addOption.id = 'option' + i;
+        addOption.textContent = options[i];
+        optionContainer.appendChild(addOption);
+
+        const newOption = document.getElementById(addOption.id);
+
+        if (newOption.textContent.length > 60) {
+            newOption.style.height = '5vw';
+        }
+
+        newOption.addEventListener('click', function () {
+            event.preventDefault();
+            updateUI(current_question_id + 1);
+        });
+    }
+}
+
+// Initial updateUI call
+const previousButton = document.getElementById('previous_button');
+previousButton.addEventListener('click', function() {
+    if(current_question_id > 0){
+        event.preventDefault();
+        updateUI(current_question_id - 1);
+    } else {
+        event.preventDefault();
+    }
+});
+
+updateUI(current_question_id);
+
+
 
 
 
